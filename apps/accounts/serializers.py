00b61ad1c103ca93c63b736850c6eb7b246e9833
Shohan <shohanulislam19892@gmail.com>
2026-06-13 15:38:22 +0600

    feat(accounts): add User and Register serializers

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'role', 'phone_number')
+        read_only_fields = ('id',)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'role', 'phone_number')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            role=validated_data.get('role', User.Role.BUSINESS_OWNER),
+            phone_number=validated_data.get('phone_number', '')
+        )
+        return user
